5fc8333e11222c87eddd2d7b772b45f638679b89
Félix VILLEMIN <fvillemin@iliad-free.fr>
2024-12-03 16:23:25 +0100

    feat: add tester client

---
+++ cmd/tester/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+
+	"github.com/v3lmx/counter/internal/tester"
+)
+
+var addr = flag.String("addr", "localhost:8000", "http service address")
+var numClients = flag.Int("numClients", 1, "Number of clients to spawn")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	go func() {
+		<-interrupt
+		cancel()
+	}()
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/connect"}
+	log.Printf("connecting to %s", u.String())
+
+	var wg sync.WaitGroup
+	wg.Add(*numClients)
+	for range *numClients {
+		go tester.Tester(ctx, &wg, u.String())
+	}
+
+	wg.Wait()
+}
---
+++ internal/tester/test.go
@@ -0,0 +1,71 @@
+package tester
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/charmbracelet/log"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	MessageIncrement = "inc"
+	MessageReset     = "res"
+	MessageCurrent   = "current"
+	MessageBest      = "best"
+)
+
+func Tester(ctx context.Context, wg *sync.WaitGroup, url string) {
+	defer wg.Done()
+	c, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, _, err := c.ReadMessage()
+			if err != nil {
+				// log.Errorf("read: %v", err)
+				return
+			}
+			// log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(MessageIncrement))
+			if err != nil {
+				log.Errorf("write: %v", err)
+				return
+			}
+		case <-ctx.Done():
+			// log.Error("done ctx")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				// log.Errorf("write close: %v", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
