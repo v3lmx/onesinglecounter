4e0381661c7ff1349a66c136020f4de57b800c4e
v3lmx <v3lmx@pm.me>
2025-02-26 16:08:30 +0100

    ci: fix

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i sh -p git gnumake go_1_23 uglifyjs podman
+#!/bin/bash
 git config --global --add safe.directory /opt/osc
 git pull
 make deploy
-#/bin/bash
+# /usr/bin/env nix-shell
+# nix-shell -i sh -p git gnumake go_1_23 uglifyjs podman
