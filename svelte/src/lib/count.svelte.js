bbd37559fb41603810db0fe9035cb87c441d080f
Félix VILLEMIN <fvillemin@iliad-free.fr>
2025-03-27 18:10:42 +0100

    fix: update best on messageCurrent

--- svelte/src/lib/count.svelte.js
+++ svelte/src/lib/count.svelte.js
@@ -4,17 +4,17 @@ let count = $state(0);
 export const best = new SvelteMap();
 
 export function getCount() {
-  return count;
+    return count;
 }
 
 export const timespansList = [
-  "minute",
-  "hour",
-  "day",
-  "week",
-  "month",
-  "year",
-  "alltime",
+    "minute",
+    "hour",
+    "day",
+    "week",
+    "month",
+    "year",
+    "alltime",
 ];
 
 const messageReset = "res";
@@ -23,56 +23,61 @@ const messageCurrent = "current";
 const messageBest = "best";
 
 for (const time of timespansList) {
-  best.set(time, 0);
+    best.set(time, 0);
 }
 
 const ws = new WebSocket("ws://localhost:10001/connect");
 ws.binaryType = "arraybuffer";
 
 export function reset() {
-  ws.send(messageReset);
+    ws.send(messageReset);
 }
 
 export function increment() {
-  ws.send(messageIncrement);
+    ws.send(messageIncrement);
 }
 
 function current() {
-  ws.send(messageCurrent);
+    ws.send(messageCurrent);
 }
 
 export function refreshBest() {
-  ws.send(messageBest);
+    ws.send(messageBest);
 }
 
 ws.onopen = (event) => {
-  current();
-  getBest();
+    current();
+    // getBest();
 };
 
 ws.onmessage = (event) => {
-  if (event.data === messageReset) {
-    count = 0;
-  }
-  if (event.data === messageIncrement) {
-    count++;
-    for (const time of timespansList) {
-      if (count > best.get(time)) {
-        best.set(time, count);
-      }
+    if (event.data === messageReset) {
+        count = 0;
     }
-  }
-  const e = event.data.split(":");
-  if (e[0] === messageCurrent) {
-    count = e[1];
-  }
-  if (e[0] === messageBest) {
-    for (let i = 1; i < e.length - 1; i += 2) {
-      const time = e[i];
-      const value = e[i + 1];
-      best.set(time, value);
+    if (event.data === messageIncrement) {
+        count++;
+        for (const time of timespansList) {
+            if (count > best.get(time)) {
+                best.set(time, count);
+            }
+        }
     }
-  }
+    const e = event.data.split(":");
+    if (e[0] === messageCurrent) {
+        count = e[1];
+        for (const time of timespansList) {
+            if (Number(count) > best.get(time)) {
+                best.set(time, count);
+            }
+        }
+    }
+    if (e[0] === messageBest) {
+        for (let i = 1; i < e.length - 1; i += 2) {
+            const time = e[i];
+            const value = e[i + 1];
+            best.set(time, value);
+        }
+    }
 
-  console.log(event.data);
+    console.log(event.data);
 };
