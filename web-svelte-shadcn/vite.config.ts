3a7faaac12fdbcad86a82c9cfbb28dc28e8213af
v3lmx <v3lmx@pm.me>
2025-06-11 11:41:35 +0200

    fix: base url

--- web-svelte-shadcn/vite.config.ts
+++ web-svelte-shadcn/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [tailwindcss(), svelte()],
+    base: '/osc/',
     resolve: {
         alias: {
             $lib: path.resolve("./src/lib"),
